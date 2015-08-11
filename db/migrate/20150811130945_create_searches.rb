12621281a7fc8eb449fb756d2502b9df440a2c62
Jona <j.christians@posteo.de>
2015-08-11 15:21:35 +0200

    Updated migration files

--- db/migrate/20150811130945_create_searches.rb
+++ db/migrate/20150811130945_create_searches.rb
@@ -1,7 +1,8 @@
 class CreateSearches < ActiveRecord::Migration
   def change
     create_table :searches do |t|
-
+      t.string :description
+      t.belongs_to :user
       t.timestamps null: false
     end
   end
--- db/migrate/20150811130953_create_offers.rb
+++ db/migrate/20150811130953_create_offers.rb
@@ -1,7 +1,8 @@
 class CreateOffers < ActiveRecord::Migration
   def change
     create_table :offers do |t|
-
+      t.string :description
+      t.belongs_to :user
       t.timestamps null: false
     end
   end
