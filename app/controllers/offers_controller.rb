4f55d9d2f8b173187e063573193a6116c8e781a0
Jona <j.christians@posteo.de>
2015-08-11 19:30:56 +0200

    Added offers views

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,28 @@
 class OffersController < ApplicationController
+
+  def index
+    @offers = Offer.all
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(signup_params)
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def signup_params
+    params.require(:offer).permit(:description)
+  end
 end
