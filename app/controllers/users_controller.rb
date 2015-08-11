2eb1884d7c263120a608e138eb5c3f62c3a144c5
Jona <j.christians@posteo.de>
2015-08-11 15:08:01 +0200

    Updated UsersController actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,25 @@ class UsersController < ApplicationController
   def index
   end
 
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(signup_params)
+    if @user.save
+      redirect_to user_path #does this work???
+    else
+      render 'new'
+  end
+
+
+
   def show
   end
+
+  private
+  def signup_params
+    params.require(:user).permit(:name)
+  end
+
 end
