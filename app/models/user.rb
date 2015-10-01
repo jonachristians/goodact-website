0338705ec9e95629c2f766a8468ef53d0893abdd
Jona <j.christians@posteo.de>
2015-10-01 19:54:50 +0200

    Change signup inputs
    Remove: Validation/Input for first and last name
    Add: Validation/Input for zipcode
    Refactor: Move signup view to a partial
    Style: Added some form styling, not done.
    
    More on style: Should be centered

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates_presence_of :username, :first_name, :last_name, :email, zip
+  validates_presence_of :username, :email, :zipcode
   has_many :offers
   has_many :searches
   has_secure_password
