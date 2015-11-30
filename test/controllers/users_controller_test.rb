9817626546470983680f312aa46c26810494108b
Jona <j.christians@posteo.de>
2015-11-30 20:40:27 +0100

    Update UsersControllerTest to only check 'new'
    Update user test to only check for the 'new' action,
    all other actions need logged in users and are therefore tested in
    the integration tests. (Eventough 'creation' action is not restricted
    either, it too is better off in the integration tests)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get new" do
+    get :new
     assert_response :success
   end
-
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
-
 end
