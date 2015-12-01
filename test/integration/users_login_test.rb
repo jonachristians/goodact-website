e606a45b5307b6323a07b4441e240f3cffbdb30d
Jona <j.christians@posteo.de>
2015-12-01 19:00:34 +0100

    MINOR in users_login test, check for flash msgs

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -22,6 +22,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
+    assert flash.any?
     assert_select "a[href=?]", login_path, false
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
@@ -29,6 +30,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not is_logged_in?
     assert_redirected_to root_path
     follow_redirect!
+    assert flash.any?
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, false
     assert_select "a[href=?]", user_path(@user), false
