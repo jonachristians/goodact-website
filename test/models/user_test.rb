4342027a69960485c8c9d4a65ad1b2ede7b931ee
Jona <j.christians@posteo.de>
2015-12-01 14:05:20 +0100

    Add user_test.rb
    Tests the essential validations on user model.
    However since user validations are very rudimentary and will
    change, don't take these tests for completed.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: "example_user", email: "test@example.com",
+                    zipcode: 12345,
+                    password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = "   "
+    assert_not @user.valid?
+  end
+
+  test "username should be unique" do
+    dup_user = @user.dup
+    @user.save
+    assert_not dup_user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+
+  test "zipcode should be present" do
+    @user.zipcode = "  "
+    assert_not @user.valid?
+  end
+
+  test "zipcode should be of length 5" do
+    [4342,632462].each do |invalid_zip|
+      @user.zipcode = invalid_zip
+      assert_not @user.valid?, "#{invalid_zip} seems be valid"
+    end
+  end
 end
