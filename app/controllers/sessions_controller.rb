33518ce26900e2c6323a9a90c4841c296e857950
Jona <j.christians@posteo.de>
2015-12-01 17:38:48 +0100

    Change login/create action to use only local var
    There is no need for an instance variable so it is changed to a local one

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ before_action :already_logged_in, only: :new
   end
 
   def create
-    @user = User.find_by_email(params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      redirect_to requested_url || @user, success: 'You successfully logged in.'
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to requested_url || user, success: 'You successfully logged in.'
     else
       flash.now[:danger] = "You're e-mail and password don't match up."
       render 'new'
